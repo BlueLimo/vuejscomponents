fadd280aee5bfc38563fdfab2b1e839b3e945c2f
Ya'kov Erick <bluelimo254@gmail.com>
2017-02-22 18:51:07 +0300

    VueJS Components

---
+++ main.js
@@ -0,0 +1,91 @@
+//create the component details
+Vue.component('detals', 
+	{
+		props: ['title', 'body', 'footer'],
+		template: `
+					<article class='articles-section'>
+						<div class='title-section'> 
+							{{ title }} 
+						</div>
+						<div class='body-section'> 
+							{{ body }} 
+						</div>
+						<div class='footer-section'> 
+							{{ footer }} 
+						</div>
+					</article>
+		`
+	});
+
+new Vue(
+{
+	el:'#main-section'
+});
+
+Vue.component('demo-grid', {
+  template: '#grid-template',
+  props: {
+    data: Array,
+    columns: Array,
+    filterKey: String
+  },
+  data: function () {
+    var sortOrders = {}
+    this.columns.forEach(function (key) {
+      sortOrders[key] = 1
+    })
+    return {
+      sortKey: '',
+      sortOrders: sortOrders
+    }
+  },
+  computed: {
+    filteredData: function () {
+      var sortKey = this.sortKey
+      var filterKey = this.filterKey && this.filterKey.toLowerCase()
+      var order = this.sortOrders[sortKey] || 1
+      var data = this.data
+      if (filterKey) {
+        data = data.filter(function (row) {
+          return Object.keys(row).some(function (key) {
+            return String(row[key]).toLowerCase().indexOf(filterKey) > -1
+          })
+        })
+      }
+      if (sortKey) {
+        data = data.slice().sort(function (a, b) {
+          a = a[sortKey]
+          b = b[sortKey]
+          return (a === b ? 0 : a > b ? 1 : -1) * order
+        })
+      }
+      return data
+    }
+  },
+  filters: {
+    capitalize: function (str) {
+      return str.charAt(0).toUpperCase() + str.slice(1)
+    }
+  },
+  methods: {
+    sortBy: function (key) {
+      this.sortKey = key
+      this.sortOrders[key] = this.sortOrders[key] * -1
+    }
+  }
+})
+
+// bootstrap the demo
+var search = new Vue({
+  el: '#search-area',
+  data: {
+    searchQuery: '',
+    gridColumns: ['name', 'regno'],
+    gridData: [
+      { name: 'Brian Kariuki', regno: 31001 },
+      { name: 'Dancun Kinuthia', regno: 31002 },
+      { name: 'Kelvin Wangari', regno: 31003 },
+      { name: 'Francis Kasiara', regno: 31004 }
+    ]
+  }
+})
